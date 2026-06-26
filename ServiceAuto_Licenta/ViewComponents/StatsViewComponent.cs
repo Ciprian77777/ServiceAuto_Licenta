230415d4c62aaa413f4bc78ecb89b4bd37a2e43c
Ciprian Banea <ciprianbanea7@gmail.com>
2026-06-26 17:15:20 +0300

    finall

--- ServiceAuto_Licenta/ViewComponents/StatsViewComponent.cs
+++ ServiceAuto_Licenta/ViewComponents/StatsViewComponent.cs
@@ -18,7 +18,7 @@ namespace ServiceAutoLicenta.ViewComponents
         public decimal VenituriLuna{get;set;}
     }
 
-    [ViewComponent(Name = "Stats")]
+    [ViewComponent(Name="Stats")]
     public class StatsViewComponent : ViewComponent
     {
         private readonly ServiceAutoLicentaContext db;
@@ -26,33 +26,35 @@ namespace ServiceAutoLicenta.ViewComponents
 
         public StatsViewComponent(ServiceAutoLicentaContext db, UserManager<IdentityUser> userManager)
         {
-            this.db = db;
-            this.userManager = userManager;
+            this.db=db;
+            this.userManager=userManager;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var userId = userManager.GetUserId(HttpContext.User);
-            if (string.IsNullOrEmpty(userId))
+            var userId=userManager.GetUserId(HttpContext.User);
+            if(string.IsNullOrEmpty(userId))
                 return View(new StatsModel());
 
-            var primaZiLuna = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+            var primaZiLuna=new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
 
-            var model = new StatsModel
+            var facturiLuna=await db.Facturi
+                .Where(x=>x.Programare.Masina.Client.UserId==userId&&x.StatusPlata==StatusPlata.Platita&&x.DataEmitere>=primaZiLuna)
+                .ToListAsync();
+
+            var model=new StatsModel
             {
-                TotalClienti= await db.Clienti.CountAsync(x => x.UserId == userId),
-                TotalMasini = await db.Masini.CountAsync(x => x.Client.UserId == userId),
-                Programate= await db.Programari.CountAsync(x => x.Masina.Client.UserId == userId && x.Status == StatusProgramare.Programata),
-                InLucru=await db.Programari.CountAsync(x => x.Masina.Client.UserId == userId && x.Status == StatusProgramare.InLucru),
-                Finalizate = await db.Programari.CountAsync(x => x.Masina.Client.UserId == userId && x.Status == StatusProgramare.Finalizata),
-                FacturiNeplatite = await db.Facturi.CountAsync(x => x.Programare.Masina.Client.UserId == userId && x.StatusPlata == StatusPlata.Neplata),
-                AlerteStoc = await db.Piese.CountAsync(x => x.UserId == userId && x.StocCurent <= x.StocMinim),
-                VenituriLuna = await db.Facturi
-                    .Where(x => x.Programare.Masina.Client.UserId == userId && x.StatusPlata == StatusPlata.Platita && x.DataEmitere >= primaZiLuna)
-                    .SumAsync(x => (decimal?)x.Total) ?? 0
+                TotalClienti=await db.Clienti.CountAsync(x=>x.UserId==userId),
+                TotalMasini=await db.Masini.CountAsync(x=>x.Client.UserId==userId),
+                Programate=await db.Programari.CountAsync(x=>x.Masina.Client.UserId==userId&&x.Status==StatusProgramare.Programata),
+                InLucru=await db.Programari.CountAsync(x=>x.Masina.Client.UserId==userId&&x.Status==StatusProgramare.InLucru),
+                Finalizate = await db.Programari.CountAsync(x=>x.Masina.Client.UserId==userId&&x.Status==StatusProgramare.Finalizata),
+                FacturiNeplatite=await db.Facturi.CountAsync(x=>x.Programare.Masina.Client.UserId==userId&&x.StatusPlata==StatusPlata.Neplata),
+                AlerteStoc=await db.Piese.CountAsync(x=>x.UserId==userId&&x.StocCurent<=x.StocMinim),
+                VenituriLuna=facturiLuna.Sum(x=>x.Total)
             };
 
-            HttpContext.Items["AlerteGlobale"] = model.AlerteStoc;
+            HttpContext.Items["AlerteGlobale"]=model.AlerteStoc;
 
             return View(model);
         }
